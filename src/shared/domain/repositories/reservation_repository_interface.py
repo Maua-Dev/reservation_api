49081af910428081f4d814d5fba191c8f38eb508
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2024-08-21 16:08:56 -0300

    feat(deleteCourt): Delete court repositoryinterface

--- src/shared/domain/repositories/reservation_repository_interface.py
+++ src/shared/domain/repositories/reservation_repository_interface.py
@@ -6,3 +6,11 @@ class IReservationRepository(ABC):
     @abstractmethod
     def create_court(self):
         pass
+
+    @abstractmethod
+    def delete_court(self):
+        '''
+        If courts exists, deletes it and returns it
+        else returns None
+        '''
+        pass
