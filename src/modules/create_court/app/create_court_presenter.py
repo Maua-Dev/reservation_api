018a17c0a2f1f46376ade433be02d96a9541c6be
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-08-27 23:53:11 -0300

    add presenter + fix court entity

---
+++ src/modules/create_court/app/create_court_presenter.py
@@ -0,0 +1,15 @@
+from .create_court_controller import CreateCourtController
+from .create_court_usecase import CreateCourtUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_reservation_repo()()
+usecase = CreateCourtUsecase(repo)
+controller = CreateCourtController(usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(request=httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    
+    return httpResponse.toDict()
