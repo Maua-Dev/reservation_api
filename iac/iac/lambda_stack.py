1365d81fb09f9c0b3f6e1fd04a16aef1592abf7d
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-09-15 21:10:17 -0300

    chore: Comment out unused lambda integrations in LambdaStack and added health check route to test running on air API

--- iac/iac/lambda_stack.py
+++ iac/iac/lambda_stack.py
@@ -49,36 +49,43 @@ class LambdaStack(Construct):
 
         self.lambda_power_tools = lambda_.LayerVersion.from_layer_version_arn(self, "Lambda_Power_Tools", layer_version_arn="arn:aws:lambda:us-east-2:017000801446:layer:AWSLambdaPowertoolsPythonV2:22")
         
-        self.create_court = self.create_lambda_api_gateway_integration(
-            module_name="create_court",
-            method="POST",
-            mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
-        )
+        # self.create_court = self.create_lambda_api_gateway_integration(
+        #     module_name="create_court",
+        #     method="POST",
+        #     mss_student_api_resource=api_gateway_resource,
+        #     environment_variables=environment_variables
+        # )
         
-        self.get_court = self.create_lambda_api_gateway_integration(
-            module_name="get_court",
-            method="GET",
-            mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
-        )
+        # self.get_court = self.create_lambda_api_gateway_integration(
+        #     module_name="get_court",
+        #     method="GET",
+        #     mss_student_api_resource=api_gateway_resource,
+        #     environment_variables=environment_variables
+        # )
         
-        self.update_court = self.create_lambda_api_gateway_integration(
-            module_name="update_court",
-            method="PUT",
-            mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
-        )
+        # self.update_court = self.create_lambda_api_gateway_integration(
+        #     module_name="update_court",
+        #     method="PUT",
+        #     mss_student_api_resource=api_gateway_resource,
+        #     environment_variables=environment_variables
+        # )
         
-        self.delete_court = self.create_lambda_api_gateway_integration(
-            module_name="delete_court",
-            method="DELETE",
-            mss_student_api_resource=api_gateway_resource,
-            environment_variables=environment_variables
-        )
+        # self.delete_court = self.create_lambda_api_gateway_integration(
+        #     module_name="delete_court",
+        #     method="DELETE",
+        #     mss_student_api_resource=api_gateway_resource,
+        #     environment_variables=environment_variables
+        # )
         
-        self.get_all_courts = self.create_lambda_api_gateway_integration(
-            module_name="get_all_courts",
+        # self.get_all_courts = self.create_lambda_api_gateway_integration(
+        #     module_name="get_all_courts",
+        #     method="GET",
+        #     mss_student_api_resource=api_gateway_resource,
+        #     environment_variables=environment_variables
+        # )
+        
+        self.health_check = self.create_lambda_api_gateway_integration(
+            module_name="health_check",
             method="GET",
             mss_student_api_resource=api_gateway_resource,
             environment_variables=environment_variables
