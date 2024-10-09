4e2b63f967887a5e4fc0ebf6608e6c6674d72ae6
Rodrigo Morales <109971335+RodrigoM2004@users.noreply.github.com>
2024-10-09 08:01:20 -0300

    fixed api gateway resource

--- iac/stacks/iac_stack.py
+++ iac/stacks/iac_stack.py
@@ -37,7 +37,7 @@ class IacStack(Stack):
                                     },
                                 )
 
-        api_gateway_resource = self.rest_api.root.add_resource("mss-reservation", default_cors_preflight_options=
+        api_gateway_resource = self.rest_api.root.add_resource("reservation-api", default_cors_preflight_options=
         {
             "allow_origins": Cors.ALL_ORIGINS,
             "allow_methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
