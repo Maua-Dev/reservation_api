76f3dd6c9764e63fd841f15c03a003833d192127
lseixas <lseixas.iorio@gmail.com>
2025-02-19 12:23:47 -0300

    update, get and delete methods of booking repo dynamo

--- src/shared/infra/repositories/booking_repository_dynamo.py
+++ src/shared/infra/repositories/booking_repository_dynamo.py
@@ -1,3 +1,5 @@
+from typing_extensions import List, Optional
+
 from src.shared.domain.entities.booking import Booking
 from src.shared.domain.enums.sport import SPORT
 from src.shared.domain.repositories.booking_repository_interface import IBookingRepository
@@ -13,8 +15,8 @@ class BookingRepositoryDynamo(IBookingRepository):
         return f'Booking'
 
     @staticmethod
-    def booking_sort_key_format(time: int) -> str:
-        return f'booking@{time}'
+    def booking_sort_key_format(booking_id: str) -> str:
+        return f'booking#{booking_id}'
 
     def __init__(self):
         self.dynamo = DynamoDatasource(
@@ -25,27 +27,69 @@ class BookingRepositoryDynamo(IBookingRepository):
             sort_key=Environments.get_envs().dynamo_sort_key,
         )
 
-
-    def create_booking(self, booking: Booking) -> Booking:
+    def create_booking(self, booking: Booking) -> Optional[Booking]:
 
         item = BookingDynamoDTO.from_entity(booking).to_dynamo()
 
         resp = self.dynamo.put_item(item,
                                     partition_key=self.booking_partition_key_format(),
-                                    sort_key=self.booking_sort_key_format(booking.start_date))
+                                    sort_key=self.booking_sort_key_format(booking.booking_id))
+
+        if 'Attributes' not in resp:
+            return None
 
         return booking
 
-    def update_booking(self, start_date: int, end_date: int, court_number: int, sport: SPORT) -> Booking:
-        pass
+    def update_booking(self,
+                       booking_id: str,
+                       start_date: int = None,
+                       end_date: int = None,
+                       court_number: int = None,
+                       sport: SPORT = None,
+                       materials: List[str] = None) -> Optional[Booking]:
 
-    def get_booking(self, booking_id: int):
-        pass
+        booking_to_update = self.get_booking(booking_id)
 
-    def delete_booking(self, booking_id: int):
-        pass
+        if booking_to_update is None:
+            return None
 
-    def get_all_bookings(self):
+        update_dict = {
+            "start_date": start_date if start_date is not None else booking_to_update.start_date,
+            "end_date": end_date if end_date is not None else booking_to_update.end_date,
+            "court_number": court_number if court_number is not None else booking_to_update.court_number,
+            "sport": sport if sport is not None else booking_to_update.sport,
+            "materials": materials if materials is not None else booking_to_update.materials
+        }
+
+        resp = self.dynamo.update_item(update_dict=update_dict,
+                                       partition_key=self.booking_partition_key_format(),
+                                       sort_key=self.booking_sort_key_format(booking_id))
+
+        if 'Attributes' not in resp:
+            return None
+
+        return BookingDynamoDTO.from_dynamo(resp['Attributes']).to_entity()
+
+    def get_booking(self, booking_id: str) -> Optional[Booking]:
+
+        dynamo_object = self.dynamo.get_item(partition_key=self.booking_partition_key_format(),
+                                           sort_key=self.booking_sort_key_format(booking_id))
+
+        if "Item" not in dynamo_object:
+            return None
+
+        return BookingDynamoDTO.from_dynamo(dynamo_object['Item']).to_entity()
+
+    def delete_booking(self, booking_id: str) -> Optional[Booking]:
+
+        delete_booking = self.dynamo.delete_item(partition_key=self.booking_partition_key_format(),
+                                                 sort_key=self.booking_sort_key_format(booking_id))
+        if "Attributes" not in delete_booking:
+            return None
+
+        return BookingDynamoDTO.from_dynamo(delete_booking['Attributes']).to_entity()
+
+    def get_all_bookings(self) -> Optional[List[Booking]]:
 
         all_bookings = []
         all_items = self.dynamo.get_all_items().get('Items')
