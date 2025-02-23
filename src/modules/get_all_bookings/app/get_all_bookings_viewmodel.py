535e6b930e07a8d846782e8098e7c4b7f2a4b812
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2025-02-22 23:10:08 -0300

    feat: get all bookings viewmodel

---
+++ src/modules/get_all_bookings/app/get_all_bookings_viewmodel.py
@@ -0,0 +1,54 @@
+from src.shared.domain.entities.booking import Booking
+from src.shared.domain.enums.sport import SPORT
+from typing import List
+
+class BookingViewModel:
+    start_date: int
+    end_date: int
+    court_number: int
+    sport: SPORT
+    user_id: str
+    booking_id: str
+    materials: List[str]
+
+    def __init__(self, booking: Booking):
+        self.start_date = booking.start_date
+        self.end_date = booking.end_date
+        self.court_number = booking.court_number
+        self.sport = booking.sport
+        self.user_id = booking.user_id
+        self.booking_id = booking.booking_id
+        self.materials = booking.materials
+
+    def to_dict(self):
+        return {
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'court_number': self.court_number,
+            'sport': self.sport.value,
+            'user_id': self.user_id,
+            'booking_id': self.booking_id,
+            'materials': self.materials
+        }
+class GetBookingViewModel:
+    booking: Booking
+    
+    def __init__(self, booking: Booking):
+        self.booking_viewmodel = BookingViewModel(booking)
+
+    def to_dict(self):
+        return{
+            'booking' : self.booking_viewmodel.to_dict()
+        }
+
+class GetAllBookingViewModel:
+    bookings: List[GetBookingViewModel]
+
+    def __init__(self, bookings: list):
+        self.bookings = [GetBookingViewModel(booking) for booking in bookings]
+
+    def to_dict(self):
+        return {
+            'bookings': [booking.to_dict() for booking in self.bookings],
+            'message': 'the bookings were retrieved'
+        }
