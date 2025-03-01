64f2a8f2cf6834875afc0233460dcb3a59bd429b
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2025-03-01 14:00:36 -0300

    Add delete bookings viewmodel

---
+++ tests/modules/delete_bookings/app/test_delete_bookings_viewmodel.py
@@ -0,0 +1,27 @@
+from src.modules.delete_bookings.app.delete_bookings_viewmodel import DeleteBookingViewModel
+from src.modules.delete_bookings.app.delete_bookings_usecase import DeleteBookingUsecase
+from src.shared.domain.entities.booking import Booking
+from src.shared.infra.repositories.booking_repository_mock import BookingRepositoryMock
+
+
+class Test_DeleteBookingViewModel:
+    def test_delete_booking_viewmodel(self):
+        repo = BookingRepositoryMock()
+        usecase = DeleteBookingUsecase(repo=repo)
+        booking = usecase(booking_id=repo.bookings[0].booking_id)
+        viewmodel = DeleteBookingViewModel(booking=booking).to_dict()
+
+        expected = {
+            'booking': {
+                'start_date': 1634576165000,
+                'end_date': 1634583365000,
+                'court_number': 1,
+                'sport': 'Tennis',
+                'user_id': 'c8435c66-13a4-4641-9d54-773b4b8ccc98',
+                'booking_id': 'b1d3bebf-dc0d-4fc1-861c-506a40cc2925',
+                'materials': ['Raquete', 'Bola', 'Rede', 'Tenis']
+            },
+            'message': 'the booking was deleted'
+        }
+
+        assert viewmodel == expected
