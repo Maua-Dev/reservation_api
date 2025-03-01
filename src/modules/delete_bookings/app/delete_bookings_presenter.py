95ac05cf5ab896969ee3f1190869f3fcfe1e257a
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2025-03-01 14:00:15 -0300

    Add delete bookings presenter

---
+++ src/modules/delete_bookings/app/delete_bookings_presenter.py
@@ -0,0 +1,15 @@
+from .delete_bookings_controller import DeleteBookingController
+from .delete_bookings_usecase import DeleteBookingUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_booking_repo()()
+usecase = DeleteBookingUsecase(repo)
+controller = DeleteBookingController(usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(request=httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    
+    return httpResponse.toDict()
