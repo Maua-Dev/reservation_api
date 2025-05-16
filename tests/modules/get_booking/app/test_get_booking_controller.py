6fa3e85d97843094ec6d7816bce5b1f45abfab22
João Antônnio Martinez <joao.antonnio.m2005@gmail.com>
2025-05-16 19:03:56 -0300

    fix: changing other routes for microservice integration to work

--- tests/modules/get_booking/app/test_get_booking_controller.py
+++ tests/modules/get_booking/app/test_get_booking_controller.py
@@ -25,7 +25,7 @@ class TestGetBookingController:
         assert response.body['booking']['end_date'] == 1634567400000
         assert response.body['booking']['court_number'] == 2
         assert response.body['booking']['sport'] == 'Football'
-        assert response.body['booking']['user_id'] == 'c8435c66-13a4-4641-9d54-773b4b8ccc98'
+        assert response.body['booking']['user_id'] == 'c07e0862-3c07-4227-ab0f-511a267cb7ff'
         assert response.body['booking']['materials'] == ['Bola', 'Chuteira']
 
     def test_get_booking_controller_missing_booking_id(self):
