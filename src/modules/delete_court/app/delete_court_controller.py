a387d0cc1dc1c2be91a1ef1ff062f2cadf03cb85
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-07-14 16:46:58 -0300

    added role validation login

--- src/modules/delete_court/app/delete_court_controller.py
+++ src/modules/delete_court/app/delete_court_controller.py
@@ -8,6 +8,7 @@ from src.shared.helpers.errors.usecase_errors import NoItemsFound
 from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
 from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, Created, InternalServerError, NotFound
 from src.shared.domain.enums.status_enum import STATUS
+from src.shared.clients.user_api_client import UserAPIClient
 
 class DeleteCourtController:
     
@@ -19,6 +20,10 @@ class DeleteCourtController:
             if request.data.get('number') is None:
                 raise MissingParameters('number')
             
+            if request.data.get('user_from_authorizer') is None:
+                raise MissingParameters('user')
+            
+            user = request.data.get("user_from_authorizer")
             court = self.usecase(number=request.data.get('number'))
             viewmodel = DeleteCourtViewModel(court)
             
--- src/modules/delete_court/app/delete_court_usecase.py
+++ src/modules/delete_court/app/delete_court_usecase.py
@@ -2,17 +2,20 @@ from src.shared.domain.entities.court import Court
 from src.shared.domain.enums.status_enum import STATUS
 from src.shared.helpers.errors.usecase_errors import DuplicatedItem
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, ForbiddenAction
 from src.shared.domain.repositories.reservation_repository_interface import IReservationRepository
 
 class DeleteCourtUsecase:
     def __init__(self, repo:IReservationRepository):
         self.repo = repo
     
-    def __call__(self, number: int):    
+    def __call__(self, number: int, role: str):    
         
         if not Court.validate_number(number):
             raise EntityError('number')
+        
+        if role != "ADMIN":
+            raise ForbiddenAction()
         
         court = self.repo.delete_court(number=number)
         
