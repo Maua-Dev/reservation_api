cf914e2c6fab6610dbfc8d65357f43c605fae1d1
Leonardo Luiz Seixas Iorio <lseixas.iorio@gmail.com>
2025-07-30 13:39:45 -0300

    fixing vent extraction

--- src/modules/create_booking/app/create_booking_presenter.py
+++ src/modules/create_booking/app/create_booking_presenter.py
@@ -2,6 +2,7 @@ from .create_booking_controller import CreateBookingController
 from .create_booking_usecase import CreateBookingUsecase
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+import json
 
 repo = Environments.get_booking_repo()()
 usecase = CreateBookingUsecase(repo)
@@ -9,10 +10,15 @@ controller = CreateBookingController(usecase)
 
 def lambda_handler(event, context):
     
-    print(event)
-    
     httpRequest = LambdaHttpRequest(data=event)
-    httpRequest.data['user_from_authorizer'] = event.get('requestContext', {}).get('authorizer', {}).get('user', None)
+    
+    user_info_string = event.get('requestContext', {}).get('authorizer', {}).get('user')
+    
+    if user_info_string:
+        httpRequest.data['user_from_authorizer'] = json.loads(user_info_string).get('user')
+    else:
+        httpRequest.data['user_from_authorizer'] = None
+
     response = controller(request=httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
 
