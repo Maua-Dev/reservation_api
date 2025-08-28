d458619350601f1822c53d594142052828107930
Victor Gasperi <victorgasperi@gmail.com>
2025-08-27 23:23:35 -0300

    feat: presenter da rota

---
+++ src/modules/get_all_bookings_grouped_by_role/app/get_all_bookings_grouped_by_role_presenter.py
@@ -0,0 +1,25 @@
+import json
+from src.modules.get_all_bookings_grouped_by_role.app.get_all_bookings_grouped_by_role_controller import GetAllBookingsGroupedByRoleController
+from src.modules.get_all_bookings_grouped_by_role.app.get_all_bookings_grouped_by_role_usecase import GetAllBookingsGroupedByRoleUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+
+repo = Environments.get_booking_repo()()
+usecase = GetAllBookingsGroupedByRoleUsecase(repo)
+controller = GetAllBookingsGroupedByRoleController(usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+
+    user_info_string = event.get('requestContext', {}).get('authorizer', {}).get('user')
+    
+    if user_info_string:
+        httpRequest.data['user_from_authorizer'] = json.loads(user_info_string).get('user')
+    else:
+        httpRequest.data['user_from_authorizer'] = None
+
+    response = controller(request=httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    
+    return httpResponse.toDict()
