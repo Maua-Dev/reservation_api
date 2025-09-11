29495febeba9d3e1f057f4c22bb5968ffe97b588
Victor Gasperi <victorgasperi@gmail.com>
2025-09-10 21:47:21 -0300

    test(repo): testes do repositorio mock

--- src/shared/infra/repositories/booking_repository_mock.py
+++ src/shared/infra/repositories/booking_repository_mock.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 from src.shared.domain.entities.booking import Booking
 from src.shared.domain.enums.sport import SPORT
+from src.shared.domain.enums.type import TYPE
 from src.shared.domain.repositories.booking_repository_interface import IBookingRepository
 from src.shared.helpers.errors.usecase_errors import ForbiddenAction
 
@@ -17,7 +18,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.TENNIS,
                 user_id='1f25448b-3429-4c19-8287-d9e64f17bc3a',
                 booking_id='b1d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Raquete', 'Bola', 'Rede', 'Tenis']
+                materials=['Raquete', 'Bola', 'Rede', 'Tenis'], 
+                booking_type=TYPE.TRAINING
             ),
 
             Booking(
@@ -27,7 +29,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.FOOTBALL,
                 user_id='c07e0862-3c07-4227-ab0f-511a267cb7ff',
                 booking_id='b2d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Bola', 'Chuteira']
+                materials=['Bola', 'Chuteira'], 
+                booking_type=TYPE.TRAINING
             ),
 
             Booking(
@@ -37,7 +40,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.BASKETBALL,
                 user_id='d351a9b1-937f-423c-a9d1-9929b5795be1',
                 booking_id='b3d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Bola']
+                materials=['Bola'], 
+                booking_type=TYPE.COMMON
             ),
 
             Booking(
@@ -47,7 +51,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.VOLLEYBALL,
                 user_id='c8435c66-13a4-4641-9d54-773b4b8ccc98',
                 booking_id='b4d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Bola', 'Rede']
+                materials=['Bola', 'Rede'], 
+                booking_type=TYPE.COMMON
             ),
 
             Booking(
@@ -57,7 +62,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.HANDBALL,
                 user_id='c8435c66-13a4-4641-9d54-773b4b8ccc98',
                 booking_id='b5d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Bola']
+                materials=['Bola'], 
+                booking_type=TYPE.TRAINING
             ),
 
             Booking(
@@ -67,7 +73,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.FUTSAL,
                 user_id='c8435c66-13a4-4641-9d54-773b4b8ccc98',
                 booking_id='b6d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Bola', 'Chuteira']
+                materials=['Bola', 'Chuteira'], 
+                booking_type=TYPE.TRAINING
             ),
 
             Booking(
@@ -77,7 +84,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.RUGBY,
                 user_id='c8435c66-13a4-4641-9d54-773b4b8ccc98',
                 booking_id='b7d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Bola', 'Tenis', 'Capacete']
+                materials=['Bola', 'Tenis', 'Capacete'], 
+                booking_type=TYPE.TRAINING
             ),
 
             Booking(
@@ -87,7 +95,8 @@ class BookingRepositoryMock(IBookingRepository):
                 sport=SPORT.PING_PONG,
                 user_id='c8435c66-13a4-4641-9d54-773b4b8ccc98',
                 booking_id='b8d3bebf-dc0d-4fc1-861c-506a40cc2925',
-                materials=['Raquete', 'Bola']
+                materials=['Raquete', 'Bola'], 
+                booking_type=TYPE.TRAINING
             ),
         ]
 
