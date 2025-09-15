44504e51d6bd3879aae8e7298053f9ae00d9d110
Victor Gasperi <victorgasperi@gmail.com>
2025-09-15 19:26:47 -0300

    fix()get_all_bookigs): remove user_id from mock tests

--- tests/modules/get_all_bookings/app/test_get_all_bookings_viewmodel.py
+++ tests/modules/get_all_bookings/app/test_get_all_bookings_viewmodel.py
@@ -18,7 +18,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634583365000,
                         'court_number': 1,
                         'sport': 'Tennis',
-                        'user_id': '1f25448b-3429-4c19-8287-d9e64f17bc3a',
                         'booking_id': 'b1d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Raquete', 'Bola', 'Rede', 'Tenis'],
                         'type': 'Training'
@@ -30,7 +29,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634567400000,
                         'court_number': 2,
                         'sport': 'Football',
-                        'user_id': 'c07e0862-3c07-4227-ab0f-511a267cb7ff',
                         'booking_id': 'b2d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Bola', 'Chuteira'],
                         'type': 'Training'
@@ -42,7 +40,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634571000000,
                         'court_number': 3,
                         'sport': 'Basketball',
-                        'user_id': 'd351a9b1-937f-423c-a9d1-9929b5795be1',
                         'booking_id': 'b3d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Bola'],
                         'type': 'Common'
@@ -54,7 +51,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634578200000,
                         'court_number': 4,
                         'sport': 'Volleyball',
-                        'user_id': 'c8435c66-13a4-4641-9d54-773b4b8ccc98',
                         'booking_id': 'b4d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Bola', 'Rede'],
                         'type': 'Common'
@@ -66,7 +62,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634581800000,
                         'court_number': 5,
                         'sport': 'Handball',
-                        'user_id': 'c8435c66-13a4-4641-9d54-773b4b8ccc98',
                         'booking_id': 'b5d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Bola'],
                         'type': 'Training'
@@ -78,7 +73,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634585400000,
                         'court_number': 5,
                         'sport': 'Futsal',
-                        'user_id': 'c8435c66-13a4-4641-9d54-773b4b8ccc98',
                         'booking_id': 'b6d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Bola', 'Chuteira'],
                         'type': 'Training'
@@ -90,7 +84,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634589000000,
                         'court_number': 5,
                         'sport': 'Rugby',
-                        'user_id': 'c8435c66-13a4-4641-9d54-773b4b8ccc98',
                         'booking_id': 'b7d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Bola', 'Tenis', 'Capacete'],
                         'type': 'Training'
@@ -102,7 +95,6 @@ class Test_GetAllBookingsViewmodel:
                         'end_date': 1634592600000,
                         'court_number': 5,
                         'sport': 'Ping Pong',
-                        'user_id': 'c8435c66-13a4-4641-9d54-773b4b8ccc98',
                         'booking_id': 'b8d3bebf-dc0d-4fc1-861c-506a40cc2925',
                         'materials': ['Raquete', 'Bola'],
                         'type': 'Training'
