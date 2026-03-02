490464355fde6bb11f7a46d1bfa4d6e9db420754
veloy25 <veloy.devmaua@gmail.com>
2026-03-02 17:57:50 -0300

    fix(update_booking_usecase): Insere a lógica de restrição de períodos de reserva acima de 3 meses.

--- src/modules/update_booking/app/update_booking_usecase.py
+++ src/modules/update_booking/app/update_booking_usecase.py
@@ -3,7 +3,7 @@ from src.shared.domain.entities.booking import Booking
 from src.shared.domain.enums.sport import SPORT
 from src.shared.domain.enums.type import BOOKING_TYPE
 from src.shared.domain.repositories.booking_repository_interface import IBookingRepository
-from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterOrderDatesError
+from src.shared.helpers.errors.domain_errors import EntityError, EntityParameterOrderDatesError, Ent
 from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound, InvalidSchedule
 
 class UpdateBookingUsecase:
@@ -61,6 +61,11 @@ class UpdateBookingUsecase:
         
         if Booking.validate_booking_type(booking_type) is False:
             raise EntityError("type")
+            
+        maxtime = start_date + (timedelta(weeks=12).total_seconds()*1000)
+        if(end_date > maxtime):
+            raise InvalidSchedulePeriod()
+        
         
         all_bookings = self.booking_repo.get_all_bookings()
 
