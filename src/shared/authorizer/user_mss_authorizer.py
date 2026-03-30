66466e9154f68eb643b67ccdc16b7862364bf788
Leo Iorio <lseixas.iorio@gmail.com>
2026-03-30 16:05:40 -0300

    removing / from url request in authorizer, as new url parameter already comes with it

--- src/shared/authorizer/user_mss_authorizer.py
+++ src/shared/authorizer/user_mss_authorizer.py
@@ -34,7 +34,7 @@ def lambda_handler(event, context):
         # Fetching the user information from the user mss
         methodArn = event["methodArn"]
         headers = {"Authorization": f"Bearer {token}"}
-        response = http.request("GET", MSS_USER_API_ENDPOINT + "/reservation-mss-user/get-user", headers=headers)
+        response = http.request("GET", MSS_USER_API_ENDPOINT + "get-user", headers=headers)
 
         # Checking if the request was successful
         if response.status != 200:
