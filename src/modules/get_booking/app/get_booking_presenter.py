9d22efde6578230e08d909728880a1885addad96
Victor Gasperi <victorgasperi@gmail.com>
2026-07-16 18:23:23 -0300

    fix(get_booking): inclui user_from_authorizer no presenter da rota

--- src/modules/get_booking/app/get_booking_presenter.py
+++ src/modules/get_booking/app/get_booking_presenter.py
@@ -3,6 +3,7 @@ from .get_booking_usecase import GetBookingUseCase
 from src.shared.environments import Environments
 from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
 from src.shared.infra.repositories.booking_repository_mock import BookingRepositoryMock
+import json
 
 repo = Environments.get_booking_repo()()
 usecase = GetBookingUseCase(repo=repo)
@@ -10,6 +11,14 @@ controller = GetBookingController(usecase=usecase)
 
 def lambda_handler(event, context):
     httpRequest = LambdaHttpRequest(data= event)
+
+    user_info_string = event.get('requestContext', {}).get('authorizer', {}).get('user')
+
+    if user_info_string:
+        httpRequest.data['user_from_authorizer'] = json.loads(user_info_string).get('user')
+    else:
+        httpRequest.data['user_from_authorizer'] = None
+
     response = controller(request = httpRequest)
     httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
 
